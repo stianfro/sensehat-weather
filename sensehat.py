9dde708ccda5aa4f891c69853d02e1c0a24e0b9c
Stian Frøystein <stianfroy@gmail.com>
2017-11-04 10:39:17 +0100

    docker

--- sensehat.py
+++ sensehat.py
@@ -4,10 +4,10 @@ import sys
 from ISStreamer.Streamer import Streamer  
   
 # --------- User Settings ---------
-CITY = "Nashville"
+CITY = "Oslo"
 BUCKET_NAME = ":partly_sunny: " + CITY + " Weather"
 BUCKET_KEY = "sensehat"
-ACCESS_KEY = "Your_Access_Key"
+ACCESS_KEY = "INITIALSLATE_TOKEN"
 SENSOR_LOCATION_NAME = "Office"
 MINUTES_BETWEEN_SENSEHAT_READS = 0.1
 # ---------------------------------
@@ -23,17 +23,17 @@ while True:
   pressure_mb = sense.get_pressure() 
 
   # Format the data
-  temp_f = temp_c * 9.0 / 5.0 + 32.0
+  temp_f = temp_c
   temp_f = float("{0:.2f}".format(temp_f))
   humidity = float("{0:.2f}".format(humidity))
   pressure_in = 0.03937008*(pressure_mb)
   pressure_in = float("{0:.2f}".format(pressure_in))
 
   # Print and stream 
-  print SENSOR_LOCATION_NAME + " Temperature(F): " + str(temp_f)
+  print SENSOR_LOCATION_NAME + " Temperature(C): " + str(temp_f)
   print SENSOR_LOCATION_NAME + " Humidity(%): " + str(humidity)
   print SENSOR_LOCATION_NAME + " Pressure(IN): " + str(pressure_in)
-  streamer.log(":sunny: " + SENSOR_LOCATION_NAME + " Temperature(F)", temp_f)
+  streamer.log(":sunny: " + SENSOR_LOCATION_NAME + " Temperature(C)", temp_f)
   streamer.log(":sweat_drops: " + SENSOR_LOCATION_NAME + " Humidity(%)", humidity)
   streamer.log(":cloud: " + SENSOR_LOCATION_NAME + " Pressure(IN)", pressure_in)
 
